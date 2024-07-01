9930709e14b678c3910b97bebe7227dfe8e4e48b
Rodrigo Valim <rodrigovalimjr2011@hotmail.com>
2024-07-01 12:09:23 -0300

    + exercícios

--- html/exercicios/form/server.js
+++ html/exercicios/form/server.js
@@ -6,7 +6,13 @@ app.use(bodyParser.urlencoded({ extended: true }))
 
 app.post('/usuarios', (req, respo) => {
     console.log(req.body)
-    respo.send('<h1>Parabéns</h1>')
+    respo.send('<h1>Parabéns. Usuário incluído</h1>')
+})
+
+app.post('/usuarios/:id', (req, respo) => {
+    console.log(req.params.id)
+    console.log(req.body)
+    respo.send('<h1>Parabéns. Usuário alterado</h1>')
 })
 
 app.listen(5500)
