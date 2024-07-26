e2d2262f3594b0568ba17724aeeaf8050dbcea8f
Rodrigo Valim <rodrigovalimjr2011@hotmail.com>
2024-07-26 11:36:59 -0300

    Concluído a sessão de Ajax

--- ajax/server.js
+++ ajax/server.js
@@ -3,6 +3,10 @@ const express = require('express')
 const app = express()
 const multer = require('multer')
 
+app.use(express.static('.'))
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+
 const storage = multer.diskStorage({
     destination: function(req, file, callback) {
         callback(null, './upload')
@@ -13,10 +17,6 @@ const storage = multer.diskStorage({
 })
 
 const upload = multer({ storage }).single('arquivo')
-
-app.use(express.static('.'))
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(bodyParser.json())
 
 app.get('/teste', (req, res) => res.send('Ok'))
 
@@ -29,4 +29,22 @@ app.post('/upload', (req, res) => {
         res.end('Concluído com sucesso.')
     })
 })
+
+app.post('/formulario', (req, res) => {
+    res.send({
+        ...req.body,
+        id: 1
+    })
+})
+
+app.get('/parOuImpar', (req, res) => {
+    // req.body
+    // req.query
+    // req.params
+    const par = parseInt(req.query.numero) % 2 === 0
+    res.send({
+        resultado: par ? 'par' : 'impar'
+    })
+})
+
 app.listen(8080, () => console.log('Executando...'))
