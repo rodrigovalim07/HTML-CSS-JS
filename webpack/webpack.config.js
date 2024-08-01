8196e7abda8a49fd8e9b9b78ac71a94f8d28bd79
Rodrigo Valim <rodrigovalimjr2011@hotmail.com>
2024-08-01 13:22:43 -0300

    Instalação de mais dependências

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,12 +1,30 @@
+const modoDev = process.env.NODE_ENV !== 'production'
 const webpack = require('webpack')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const TerserPlugin = require('terser-webpack-plugin')
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin')
 
 module.exports = {
-    mode: 'development',
+    mode: modoDev ? 'development' : 'production',
     entry: './src/principal.js',
     output: {
         filename: 'principal.js',
         path: __dirname + '/public'
+    },
+    optimization: {
+        minimizer: [
+            new TerserPlugin({
+                cache: true,
+                parallel: true,
+                terserOptions: {
+                    compress: {
+                        drop_console: true,
+                        ecma: 6
+                    },
+                },
+            }),
+            new OptimizeCSSAssetsPlugin({})
+        ]
     },
     plugins: [
         new MiniCssExtractPlugin({
@@ -22,6 +40,9 @@ module.exports = {
                 'css-loader', // interpreta @import, url()...
                 'sass-loader',
             ]
+        }, {
+            test: /\.(png|svg|jpg|gif)$/,
+            use: ['file-loader']
         }]
     }
 }
