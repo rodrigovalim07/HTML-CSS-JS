ab7c62d19e5efa58bc0858bc48d97a764685314e
Rodrigo Valim <rodrigovalimjr2011@hotmail.com>
2024-08-02 15:08:39 -0300

    Encerrado o projeto galeria

---
+++ bootstrap/projeto-galeria/src/index.js
@@ -0,0 +1,10 @@
+// Arquivos SASS
+import './scss/index.scss'
+
+// Dependências
+import 'jquery'
+import 'bootstrap'
+
+// Arquivos JS
+import './js/core/includes'
+import './js/plugins/cityButtons'
